8fb7a7e7c3b842b8918e868bab49ae323e487864
Kyle <garnerk10@gmail.com>
2022-08-10 12:10:35 -0400

    added throw throw burrito to partyGame array

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const twoToFour = [`Azul`, `7 Wonders Duel`, `Oceans`, `Everdell`, `Settlers of 
 
 const fivePlus = [`Betrayal at House on the Hill`, `7 Wonders`, `Everdell Expansion`, `The Crew`, `Mysterium`];
 
-const partyGame = [`Wavelength`, `Pictionary`, `Codenames`, `One Night: Werewolf`];
+const partyGame = [`Wavelength`, `Pictionary`, `Codenames`, `One Night: Werewolf`, `Throw Throw Burrito`];
 
 const getGameRecs = () => {
     let recOne = twoToFour[(Math.floor(Math.random()*5))];
     let recTwo = fivePlus[(Math.floor(Math.random()*5))];
-    let recThree = partyGame[(Math.floor(Math.random()*4))];
+    let recThree = partyGame[(Math.floor(Math.random()*5))];
 
     console.log(`2 to 4 player Game: ${recOne}
 5+ Player Game: ${recTwo}
