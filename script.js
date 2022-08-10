db222fd7d2c36b71166e22e92b0112492ae6b03f
Kyle <garnerk10@gmail.com>
2022-08-10 12:06:42 -0400

    added script to repo

---
+++ script.js
@@ -0,0 +1,17 @@
+const twoToFour = [`Azul`, `7 Wonders Duel`, `Oceans`, `Everdell`, `Settlers of Catan`];
+
+const fivePlus = [`Betrayal at House on the Hill`, `7 Wonders`, `Everdell Expansion`, `The Crew`, `Mysterium`];
+
+const partyGame = [`Wavelength`, `Pictionary`, `Codenames`, `One Night: Werewolf`];
+
+const getGameRecs = () => {
+    let recOne = twoToFour[(Math.floor(Math.random()*5))];
+    let recTwo = fivePlus[(Math.floor(Math.random()*5))];
+    let recThree = partyGame[(Math.floor(Math.random()*4))];
+
+    console.log(`2 to 4 player Game: ${recOne}
+5+ Player Game: ${recTwo}
+Party Game: ${recThree}`)
+};
+
+getGameRecs();
